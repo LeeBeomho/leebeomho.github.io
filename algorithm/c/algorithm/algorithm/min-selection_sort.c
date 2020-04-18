06c82c805dfb756cc20876785e4a5244cf6e7949
LeeBeomho <201501599@inu.ac.kr>
2020-04-18 20:06:39 +0900

    add c algorithm

---
+++ algorithm/c/algorithm/algorithm/min-selection_sort.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main() {
+
+	int array[] = { 5, 2, 4, 6, 1, 3 };
+
+	for (int i = 0; i < (sizeof(array) / sizeof(int)); i++) {
+		printf("%d ", array[i]);
+	}
+	printf("\n");
+
+	int array_size = sizeof(array) / sizeof(int);
+
+	MinSelectArrayInt(array, array_size);
+
+	
+	for (int i = 0; i < (sizeof(array) / sizeof(int)); i++) {
+		printf("%d " , array[i]);
+	}
+	printf("최종");
+
+	return 0;
+}
+
+int MinSelectArrayInt(int array[], int array_size) {
+	int min_num, min_num_index;
+	int temp;
+	
+	int another_index;
+	
+	for (int insert_index = 0; insert_index < array_size-1; insert_index++) {
+		min_num = array[insert_index];
+		min_num_index = insert_index;
+
+		for (another_index = insert_index; another_index < array_size; another_index++) {
+			if (min_num > array[another_index]) {
+				min_num = array[another_index];
+				min_num_index = another_index;
+			}
+		}
+		printf("%d번 인덱스 부터의 최솟값 %d은 인덱스 %d번 이다.\n", insert_index, min_num, min_num_index);
+		printf("%d번 인덱스 값과 %d번 인덱스 값 교환\n", insert_index, min_num_index);
+
+
+		temp = array[insert_index];
+		array[insert_index] = array[min_num_index];
+		array[min_num_index] = temp;
+
+		for (int i = 0; i < array_size; i++) {
+			printf("%d ", array[i]);
+		}
+		printf("%d번째 정렬 \n", insert_index +1);
+	}
+
+	return 0;
+
+}
+
+
---
+++ algorithm/java/algorithm/src/insertion/sort/DescendingInsertionSort.java
@@ -0,0 +1,49 @@
+package insertion.sort;
+import java.util.Arrays; 
+
+//내림차순
+public class DescendingInsertionSort {
+	public static void main(String[] args) {
+		int[] array= {5, 2, 4, 6, 1, 3};
+		//int[] array= {1, 2, 3, 4, 5, 6}; //최악의 경우
+		//int[] array= {6, 5, 4, 3, 2, 1}; //최선의 경우
+		
+		System.out.println(Arrays.toString(array));
+		
+		descendingInsertionSortArrayInt(array);
+		
+		System.out.println(Arrays.toString(array)+" 최종");
+	}
+	public static void descendingInsertionSortArrayInt(int[] array) {
+		int key; //정렬할때 배열을 알맞은 위치에 삽일하게 해주는 key
+		System.out.println();
+		// 맨 앞에 값은 이미 삽입했고 그뒤에 값들을 삽입하기 때문에 n-1번 시도함
+		for(int i, j=1; j<array.length; j++) { // j-1 번 인덱스 앞 뒤로 삽입 할거기 때문에 j는 1부터 시작
+			key=array[j];
+			
+			i=j-1; // 인덱스 j번 앞 배열은 정렬되있음 그것이 인덱스 i번까지임
+			
+			System.out.println(j+"번 인덱스 값 "+key+"가 key 이다. ");
+			
+			System.out.println(i+"번 인뎃스 값 "+array[i]+"와 비교 .");
+			
+			while((i>=0)&&(array[i]<key)) { // i는 인덱스 번호가 0이상이고 정렬된 배열이 키값보다 크냐?
+				System.out.print("key 값이 더 크다."+i+"번 인뎃스 값 "+(i+1)+"번 인덱스로 민다.");
+				
+				array[i+1]=array[i]; //key 값이 크기 때문에 key값보다 작은 값들을 뒤로 미는 작업
+				i=i-1; // key를 더 큰 값들과 비교하기 위해 i를 줄인다.
+				
+				if(i!=-1)System.out.println(i+"번 인뎃스 값 "+array[i]+"와 비교.");
+				else System.out.println("0번 인덱스에 삽입"); 
+			}
+			if(i!=-1) System.out.println(i+"번 인덱스 값 "+array[i]+"보다 key가 작다. ");
+			array[i+1]=key; // key값이 i 인덱스 갑보다 작기 때문에 i 인덱스 위에 삽입함
+			System.out.println((i+1)+"번 인덱스  key 삽입");
+			
+			System.out.println(Arrays.toString(array)+" "+j+"번 정렬");
+			System.out.println();
+		}
+		
+		return;
+	}
+}
